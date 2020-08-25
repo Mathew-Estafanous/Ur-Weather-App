2bd6d8a4cbcf910df205c2f2996b17f6a8abe839
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2020-08-25 09:15:53 -0400

    Added DailyWeatherService Tests & Removed unused tests.

---
+++ src/test/java/com/urweather/app/backend/service/DailyWeatherServiceTest.java
@@ -0,0 +1,48 @@
+package com.urweather.app.backend.service;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+
+import com.google.gson.JsonSyntaxException;
+import com.urweather.app.backend.entity.GeoLocationObject;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DailyWeatherServiceTest {
+
+    @Autowired
+    private DailyWeatherService dailyWeatherService;
+    @Autowired
+    private GeoLocationService geoLocationService;
+
+    @Test
+    public void dailyWeatherRepositoryIsFilledProperlly() {
+        int dailyWeatherCount = dailyWeatherService.getListOfDailyWeatherEntities(6).size();
+        Assert.assertNotEquals(0, dailyWeatherCount);
+    }
+
+    @Test(expected = Exception.class)
+    public void failedWhenGivenNullObject() throws Exception {
+        dailyWeatherService.createDailyWeatherInformation(null);
+        dailyWeatherService.createDailyWeatherInformation(new GeoLocationObject());
+    }
+
+    @Before
+    public void creatGeoLocationAndCallDailyService() {
+        GeoLocationObject geoLocationObj = new GeoLocationObject();
+        try {
+            geoLocationObj = geoLocationService.getGeoLocationObjFromString("Richmond Hill, CA");
+            dailyWeatherService.createDailyWeatherInformation(geoLocationObj);
+        } catch (JsonSyntaxException | InputMismatchException | IndexOutOfBoundsException | IOException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+}
